7d24096c0e99d4398a3b159244c94bf50836fefa
raghu <raghavendravenkatesh1@gmail.com>
2024-12-06 18:16:53 +0530

    Orders

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const { productRouter } = require('./routes/products')
-const app = express();
-const PORT = process.env.PORT || 3000;
 const cors = require('cors');
+const connectDB = require('./config/db');
+const { productRouter } = require('./routes/products');
 
-//CORS
-app.use(cors());
+const app = express();
+const PORT = process.env.PORT || 8080; 
+
+// CORS
 app.use(cors({
     origin: 'http://localhost:5173' // Allow
 }));
 
 app.use(express.json());
 
-// MongoDB
-mongoose.connect('mongodb+srv://raghavendravenkatesh1:Prajnan@cluster0.fywx6.mongodb.net/products')
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.error(err));
+// Connect to MongoDB
+connectDB();
 
-app.use('/v1/products', productRouter)
+app.use('/v1/products', productRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
